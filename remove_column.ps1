c0aedac34fbe841e8532e60fff74d57e0145a2c8
Psifrous <36012704+psifrous@users.noreply.github.com>
2019-01-17 21:58:20 +0530

    Add files via upload

---
+++ remove_column.ps1
@@ -0,0 +1,24 @@
+﻿$folderPath = 'C:\Users\Beautisaurus\Desktop\gwf\'
+
+# Destination folder for the new files. Be sure to include a backslash after the folder.
+$folderPathDest = 'C:\Users\Beautisaurus\Desktop\fgg\'
+
+# Desired columns - enclose in single quotes.
+$desiredColumns = "'EEG Fpz-Cz'"
+
+# Generate a list of all files in the folder and pipe it to ForEach-Object
+Get-ChildItem $folderPath -Name |
+
+# Loop through each file
+ForEach-Object { 
+
+    # Combines source folder path and file name
+    $filePath = $folderPath + $_
+
+    # Combines destination folder and file name
+    $filePathdest = $folderPathDest + $_
+
+    # Imports CSV file, selects desired columns, and then exports as CSV to the desired destination
+    Import-Csv $filePath | Select -Skip 1 |
+    Export-Csv -Path $filePathDest –NoTypeInformation
+    }
